63df324a92d62b8e3af6e384c432c601b0611fa8
CMON_ <renek89@gmail.com>
2017-03-16 18:02:52 +0100

    21703016
    Before lesson

---
+++ OB_20170316/src/main.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Rene on 16.03.2017.
+ */
+public class main {
+
+}
---
+++ test/src/main.java
@@ -0,0 +1,10 @@
+import javax.sound.midi.SysexMessage;
+
+/**
+ * Created by Rene on 09.02.2017.
+ */
+class TestApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
